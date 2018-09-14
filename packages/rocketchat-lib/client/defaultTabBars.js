5fae817633f95f789b560d60a97f2116fdca74b5
divergefx <support@divergefx.com>
2018-09-14 17:00:40 -0400

    Update defaultTabBars.js

--- packages/rocketchat-lib/client/defaultTabBars.js
+++ packages/rocketchat-lib/client/defaultTabBars.js
@@ -4,7 +4,7 @@ RocketChat.TabBar.addButton({
 	i18nTitle: 'Search_Messages',
 	icon: 'magnifier',
 	template: 'RocketSearch',
-	order: 1,
+	order: 1
 });
 
 RocketChat.TabBar.addButton({
@@ -13,7 +13,7 @@ RocketChat.TabBar.addButton({
 	i18nTitle: 'User_Info',
 	icon: 'user',
 	template: 'membersList',
-	order: 2,
+	order: 4
 });
 
 RocketChat.TabBar.addButton({
@@ -22,11 +22,11 @@ RocketChat.TabBar.addButton({
 	i18nTitle: 'Members_List',
 	icon: 'team',
 	template: 'membersList',
-	order: 2,
+	order: 4,
 	condition() {
 		const rid = Session.get('openedRoom');
 		const room = RocketChat.models.Rooms.findOne({
-			_id: rid,
+			_id: rid
 		});
 
 		if (!room || !room.broadcast) {
@@ -34,7 +34,7 @@ RocketChat.TabBar.addButton({
 		}
 
 		return RocketChat.authz.hasAllPermission('view-broadcast-member-list', rid);
-	},
+	}
 });
 
 RocketChat.TabBar.addButton({
@@ -43,7 +43,7 @@ RocketChat.TabBar.addButton({
 	i18nTitle: 'Add_users',
 	icon: 'user-plus',
 	template: 'inviteUsers',
-	order: 2,
+	order: 5
 });
 
 
@@ -53,7 +53,7 @@ RocketChat.TabBar.addButton({
 	i18nTitle: 'Room_uploaded_file_list',
 	icon: 'clip',
 	template: 'uploadedFilesList',
-	order: 3,
+	order: 6
 });
 
 RocketChat.TabBar.addButton({
@@ -62,5 +62,5 @@ RocketChat.TabBar.addButton({
 	i18nTitle: 'Keyboard_Shortcuts_Title',
 	icon: 'keyboard',
 	template: 'keyboardShortcuts',
-	order: 4,
+	order: 7
 });
