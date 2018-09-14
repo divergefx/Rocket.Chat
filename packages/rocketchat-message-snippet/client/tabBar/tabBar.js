f96dcae374d9fe2c517d2258c279d0cc8ec31560
divergefx <support@divergefx.com>
2018-09-14 16:40:03 -0400

    Update tabBar.js

--- packages/rocketchat-message-snippet/client/tabBar/tabBar.js
+++ packages/rocketchat-message-snippet/client/tabBar/tabBar.js
@@ -3,11 +3,11 @@ Meteor.startup(function() {
 		if (RocketChat.settings.get('Message_AllowSnippeting')) {
 			RocketChat.TabBar.addButton({
 				groups: ['channel', 'group', 'direct'],
-				id: 'Twitter',
-				i18nTitle: 'Twitter',
-				icon: 'twitter',
+				id: 'snippeted-messages',
+				i18nTitle: 'snippet-message',
+				icon: 'code',
 				template: 'snippetedMessages',
-				order: 3
+				order: 20,
 			});
 		} else {
 			RocketChat.TabBar.removeButton('snippeted-messages');
